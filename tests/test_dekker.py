a29c90d2d7fc53ec43b82c808e926c2dda37018e
Luan Marc <75227668+luanmarc@users.noreply.github.com>
2021-10-20 17:09:07 -0300

    Update test_dekker.py

--- tests/test_dekker.py
+++ tests/test_dekker.py
@@ -1,7 +1,7 @@
 """
 Authors: Luiz Gustavo Mugnaini Anselmo (nUSP: 11809746)
          Victor Manuel Dias Saliba     (nUSP: 11807702)
-         Luan Marc                     (nUSP: 11809090)
+         Luan Marc Suquet Camargo      (nUSP: 11809090)
 
 Computacao III (CCM): EP 1
 Test for Dekker method for finding roots of a given function.
