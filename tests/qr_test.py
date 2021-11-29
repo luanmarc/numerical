bfb9982d57c804dca99fff4e78f55df919283360
Luiz Mugnaini <luizmugnaini@gmail.com>
2021-11-29 18:49:20 -0300

    Add feature: interactive test for QR factorization
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

--- tests/qr_test.py
+++ tests/qr_test.py
@@ -8,68 +8,100 @@ Computacao III (CCM): EP 3 QR Factorization test
 from numerical.matrix.linear_sys import Qr
 import numpy as np
 
+class Col:
+    QST = "\033[1;32m::\033[0m"
+    SOL = "\033[1;35m=>\033[0m"
+    INF = "\033[1;34m->\033[0m"
+    WAR = "\033[4;33m"
+    ERR = "\033[1;31m"
+    TITL = "\033[1;35m"
+    RES = "\033[0m"
 
-def example1():
-    print(f"{' Example 1 ':-^79}")
-    mat = np.array([
-        [1., 2, -1, 3],
-        [2, 3, 9, 16],
-        [-2, 0, 4, -1],
-        [2, 11, 5, 3],
-        [0, 2, -1, 4],
-        [1, -3, 1, 7],
-    ])
-    vec = np.array([1., 0, 1, 0, -1, 1])
+def example1(path: str):
+    print(f"\n{Col.TITL}{' Example 1 ':-^79}{Col.RES}\n")
+    if not path:
+        path = "Exemplo1.txt"
+    file = open(path, "r")
+    ls = file.readlines()
+
+    # Read data from `file`
+    shape = ls[0]
+    m, n = map(int, shape.replace("\n", "").split(" "))
+    data = np.zeros((m, n + 1))
+    for i in range(1, m + 1):
+        data[i - 1, :] = list(map(float, ls[i].replace("\n", "").split(" ")))
+
+    # Solve system
+    mat, vec = data[:, :-1], data[:, -1]
     sol = Qr.solve(mat, vec)
-    npsol, a, b, c = np.linalg.lstsq(mat, vec, rcond=None)
-    print(f"solution:\n{sol}\n")
-    print(f"numpy solution:\n{npsol}\n\n\n")
-
-
-def example2():
-    print(f"{' Example 2 ':-^79}")
-
-    year = np.array(
-        [1900.0, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000]
+    res = vec - np.matmul(mat, sol)
+    print(
+        f"{Col.INF} Given the system (A b):\n{data}\n\n"
+        f"{Col.SOL} The obtained solution is:\n{sol}\n\n"
+        f"{Col.INF} Residue norm is: {np.linalg.norm(res)}"
     )
-    pop = np.array([
-        75995.0, 91972, 105711, 123203, 131669, 150697,
-        179323, 203212, 226505, 249633, 281422,
-    ])
 
-    mat = np.ndarray((11, 4))
 
-    for i in range(11):
+def example2(path: str):
+    print(f"\n{Col.TITL}{' Example 2 ':-^79}{Col.RES}\n")
+    if not path:
+        path = "Exemplo2.txt"
+    file = open(path, "r")
+    ls = file.readlines()
+
+    # Read input data from `file`
+    m = int(ls[0].replace("\n", ""))
+    data = np.zeros((m, 2))
+    for i in range(1, m + 1):
+        data[i - 1, :] = list(map(float, ls[i].replace("\n", "").split(" ")))
+    year = data[:, 0]
+    pop = data[:, 1]
+    # TODO: find yy_i and insert it into the zip for printing!!
+    print(
+        f"{Col.INF} From the input we got the following data:\n"
+        f"[(t_i, y_i, yy_i)] =\n{[x for x in zip(year, pop)]}\n\n"
+    )
+
+    mat = np.ndarray((m, 4))
+    for i in range(m):
         mat[i, 0] = 1
         mat[i, 1] = (year[i] - 1950) / 50
         mat[i, 2] = ((year[i] - 1950) / 50) ** 2
         mat[i, 3] = ((year[i] - 1950) / 50) ** 3
 
     sol = Qr.solve(mat, pop)
-    npsol, a, b, c = np.linalg.lstsq(mat, pop, rcond=None)
-    print(f"solution:\n{sol}\n")
-    print(f"numpy solution:\n{npsol}\n\n\n")
-
     value_at_2010 = sol[0]
     for i in range(1, 4):
         value_at_2010 += sol[i] * ((6 / 5) ** i)
+    res = pop - np.matmul(mat, sol)
 
-    print(f"Approximation for the population in 2010:\n{value_at_2010}\n\n\n")
-
-
-def example3():
-    print(f"{' Example 3 ':-^79}")
-
-    x_coor = np.array(
-        [1.02, 0.95, 0.87, 0.77, 0.67, 0.56, 0.44, 0.3, 0.16, 0.01]
-    )
-    y_coor = np.array(
-        [0.39, 0.32, 0.27, 0.22, 0.18, 0.15, 0.13, 0.12, 0.13, 0.15]
+    print(
+        f"{Col.INF} Given the system (A b):\n{np.column_stack((mat, pop))}\n\n"
+        f"{Col.INF} The obtained solution is:\n{sol}\n\n"
+        f"{Col.INF} Residue norm is: {np.linalg.norm(res)}\n\n"
+        f"{Col.SOL} Approximate population in 2010: {value_at_2010}"
     )
 
-    mat = np.ndarray((10, 5))
-    f = np.array([-1., -1, -1, -1, -1, -1, -1, -1, -1, -1])
 
+
+def example3(path: str):
+    print(f"\n{Col.TITL}{' Example 3 ':-^79}{Col.RES}\n")
+    if not path:
+        path = "Exemplo3.txt"
+    file = open(path, "r")
+    ls = file.readlines()
+
+    # Read input data from `file`
+    m = int(ls[0].replace("\n", ""))
+    data = np.zeros((m, 2))
+    for i in range(1, m + 1):
+        data[i - 1, :] = list(map(float, ls[i].replace("\n", "").split(" ")))
+    x_coor = data[:, 0]
+    y_coor = data[:, 1]
+
+    mat = np.ndarray((m, 5))
+
+    f = np.full(shape=m, fill_value=-1.0, dtype=float)
     for i in range(10):
         mat[i, 0] = x_coor[i] ** 2
         mat[i, 1] = x_coor[i] * y_coor[i]
@@ -78,22 +110,60 @@ def example3():
         mat[i, 4] = y_coor[i]
 
     sol = Qr.solve(mat, f)
-    npsol, a, b, c = np.linalg.lstsq(mat, f, rcond=None)
-    print(f"solution:\n{sol}\n")
-    print(f"numpy solution:\n{npsol}\n\n\n")
+    res = f - np.matmul(mat, sol)
+    print(
+        f"{Col.INF} Given the system (A b):\n{np.column_stack((mat, f))}\n\n"
+        f"{Col.SOL} The obtained solution is:\n{sol}\n\n"
+        f"{Col.INF} Residue norm is: {np.linalg.norm(res)}"
+    )
 
 
 # TODO: find some interesting application of qr factorization algorithm
-def example4():
-    print(f"{' Example 4 ':-^79}")
-    print("TODO: implement")
+def example4(path: str):
+    print(f"\n{Col.TITL}{' Example 4 ':-^79}{Col.RES}\n")
+    if not path:
+        path = "Exemplo4.txt"
+    file = open(path, "r")
+    ls = file.readlines()
+    print(f"{Col.ERR}[*] TODO: implement{Col.RES}")
 
 
 def main():
-    example1()
-    example2()
-    example3()
-    example4()
+    print(f"{Col.TITL}{' QR factorization ':-^79}{Col.RES}")
+    stop = False
+    while not stop:
+        ans = input(
+            f"\n{Col.QST} Examples:\n"
+            "   [1] Linear System;\n"
+            "   [2] Populational growth;\n"
+            "   [3] Planetary orbit;\n"
+            f"   [4] {Col.WAR}****TODO****{Col.RES}\n"
+            f"{Col.QST} Which one would you like to run? [1, 2, 3, 4] "
+        )
+        examples = {"1": example1, "2": example2, "3": example3, "4": example4}
+
+        if ans in examples:
+            arg = input(
+                f"\n{Col.QST} Please provide the path for the input file\n"
+                f"{Col.INF} "
+            )
+            examples[ans](arg)
+        else:
+            ans_err = input(
+                f"\n{Col.ERR}[*] Unfortunately such example {ans} "
+                f"is not available.{Col.RES}\n"
+                f"{Col.QST} Would you like to quit? [Y/n] "
+            )
+            if not ans_err or ans_err in ["y", "Y"]:
+                stop = True
+            else:
+                continue
+
+        ans_ex = input(
+            f"\n{Col.QST} Would you like to test another example? [Y/n] "
+        )
+        if ans_ex in ["n", "N"]:
+            stop = True
 
 
 if __name__ == "__main__":
