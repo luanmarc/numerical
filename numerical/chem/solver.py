21b568eb9adba8b6001c8e4d1e48cdfacfc1df0e
Luiz Mugnaini <luizmugnaini@gmail.com>
2022-07-09 21:42:03 -0300

    Dimerisation kinetics
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

--- numerical/chem/solver.py
+++ numerical/chem/solver.py
@@ -11,8 +11,8 @@ from random import random
 import math
 
 
-def plot(ts, xs, ytitle: str, title: str):
-    c = ["green", "brown", "blue", "orange"]
+def plot(ts, xs, ytitle: str, title: str, labels: list[str]):
+    c = ["green", "orange", "magenta", "black"]
     font = { "family": "monospace", "size": 20 }
 
     # Plot the evolution of each species
@@ -23,6 +23,7 @@ def plot(ts, xs, ytitle: str, title: str):
     plt.title(title, font)
     plt.xlabel("time", font)
     plt.ylabel(ytitle, font)
+    plt.legend(labels)
     plt.show()
 
 
@@ -30,9 +31,12 @@ def ssa(
     x0: list[int],
     r: list[list[int]],
     a: list[Callable[[list[int]], float]],
-    title: str,
+    vol: float,
     tspan: float=50,
-    t0: float=0
+    t0: float=0,
+    title: str="",
+    _plot: bool=True,
+    labels: list[str]=[]
 ):
     """Stochastic simulation algorithm:
     `x0`: initial state of the system
@@ -41,12 +45,16 @@ def ssa(
     `tspan`: time span for the simulation (optional, defaults to 50)
     `t0`: starting time (optional, defaults to 0)
     """
+    C = 6.023e23 * vol # Used to convert from #molecules -> concentration
     N = len(x0) # Number of species
     M = len(r) # Number of possible reactions
     ts = [t0] # Time recording
     xs = [x0] # State recording
-    def _ssa(x: list[int], t: float):
-        """Auxiliar recursive function for ssa"""
+    ys = [[x0[j]/C for j in range(N)]] # Records concentrations
+    t = t0
+
+    while t - t0 < tspan:
+        x = xs[-1]
         ax = [_a(x) for _a in a]
         e1, e2 = (random(), random())
         sum_ax = sum(ax)
@@ -68,14 +76,14 @@ def ssa(
         t = t + tau
         xs.append(x)
         ts.append(t)
+        ys.append([xs[-1][j]/C for j in range(N)])
 
-        if t - t0 < tspan:
-            _ssa(x, t)
+    if _plot:
+        print("Plotting data-points for stochastic model...")
+        plot(ts, xs, "Nr. molecules", title, labels)
+        plot(ts, ys, "Concentration", title, labels)
 
-    print("Calculating stochastic interactions over time...")
-    _ssa(x0, t0)
-    print("Plotting data-points for stochastic model...")
-    plot(ts, xs, "Nr. molecules", title)
+    return ts, xs, ys
 
 
 def elmaru(
@@ -83,9 +91,11 @@ def elmaru(
     r: list[list[int]],
     a: list[Callable[[list[int]], float]],
     vol: float,
-    title: str,
     tspan: float=50,
-    L: int=250
+    L: int=250,
+    title: str="",
+    _plot: bool=True,
+    labels: list[str]=[],
 ):
     """Euler-Maruyama method for Chemical Langevin Equation
     `x0`: initial state of the system
@@ -103,10 +113,10 @@ def elmaru(
     # Initial setup
     xs = [x0] # State recording
     ys = [[x0[j]/C for j in range(N)]] # Records concentrations
-    ts = [n * tau for n in range(L + 1)] # Time records
+    ts = [n * tau for n in range(L)] # Time records
 
     # Simulations
-    for _ in range(L):
+    for _ in range(L - 1):
         # Propensities for the current realisation
         axs = [_a(xs[-1]) for _a in a]
 
@@ -127,6 +137,9 @@ def elmaru(
         xs.append([last[j] + rsum[j] for j in range(N)])
         ys.append([xs[-1][j]/C for j in range(N)])
 
-    print("Plotting the evolution of the Euler-Maruyama model...")
-    plot(ts, xs, "Nr. molecules", title)
-    plot(ts, ys, "concentration", title)
+    if _plot:
+        print("Plotting the evolution of the Euler-Maruyama model...")
+        plot(ts, xs, "Nr. molecules", title, labels)
+        plot(ts, ys, "concentration", title, labels)
+
+    return ts, xs, ys
--- tests/solver_test.py
+++ tests/solver_test.py
@@ -6,19 +6,100 @@ Authors: Luiz Gustavo Mugnaini Anselmo (nUSP: 11809746)
 Computacao IV (CCM): Chemistry project
 """
 from numerical.chem.solver import ssa, elmaru
+import matplotlib.pyplot as plt
 import numpy as np
 
-def dimkin():
-    y0 = [5e-7, 0]
-    vol = 1e-15
-    def dimerisation():
-        k1 = 5e5 # 2 P -> P2
 
-    def dissociation():
-        k2 = 0.2 # P2 -> 2 P
+font = { "family": "monospace", "size": 20 }
+
+
+def dimkin():
+    ls = ["P", "$P_2$"]
+    x0 = [301, 0]
+    N = 2
+    vol = 1e-15
+    r = [
+        [-2, 1], # Dimerisation
+        [2, -1] # Dissociation
+    ]
+    c1, c2 = 1.66e-3, 0.2
+
+    def dim(x):
+        """Dimerisation propensity"""
+        return c1 * x[0] * (x[0] - 1)/2
+
+    def diss(x):
+        """Dissociation propensity"""
+        return c2 * x[1]
+
+    ssa(x0, r, [dim, diss], vol, title="Dimerisation kinetics: SSA", tspan=10, labels=ls)
+    elmaru(x0, r, [dim, diss], vol,
+           title="Dimerisation kinetics: Euler-Maruyama", tspan=10, L=500, labels=ls)
+
+    print("Plotting the overlay of 20 runs for the protein...")
+    for _ in range(20):
+        ts, xs, _ = ssa(x0, r, [dim, diss], vol, tspan=10, _plot=False)
+        plt.plot(ts, [x[0] for x in xs], color="black", label="P")
+    plt.title("Protein evolution: 20 runs")
+    plt.ylim(0, x0[0])
+    plt.xlabel("Time")
+    plt.ylabel("Nr. molecules")
+    plt.show()
+
+    runs = 10000
+    print(f"Running {runs} stochastic simulations...")
+    ps = []
+    for _ in range(runs):
+        _, xs, _ = ssa(x0, r, [dim, diss], vol, tspan=10, _plot=False)
+        ps.append(xs[-1][0])
+
+    print("Plotting the density histogram of the protein at time t = 10...")
+    plt.hist(ps, bins=int(185/5), density=True,
+             edgecolor="black", color="orange")
+    plt.title("Density histogram of P: SSA")
+    plt.xlabel("P(10)", font)
+    plt.ylabel("Density", font)
+    plt.show()
+
+    runs = 1000
+    print(f"Running {runs} Euler-Maruyama simulations...")
+    _L = 500
+    _tspan = 10
+    tau = _tspan / _L
+    ts = np.array([n * tau for n in range(_L)])
+    pem = np.zeros(_L)
+    for _ in range(runs):
+        _, xs, _ = elmaru(x0, r, [dim, diss], vol, tspan=_tspan, L=_L, _plot=False)
+        xs = np.array(xs)
+        for t in range(_L):
+            pem[t] += xs[t, 0]
+    pem *= 1/runs # Take mean
+
+    # Calculate standard deviation
+    std_run = 3
+    rec = np.zeros((std_run, _L))
+    for run in range(std_run):
+        _, xs, _ = elmaru(x0, r, [dim, diss], vol, tspan=_tspan, L=_L, _plot=False)
+        xs = np.array(xs)
+        for t in range(_L):
+            rec[run, t] = xs[t, 0]
+    std = np.zeros(_L)
+    for t in range(_L):
+        std[t] = np.std(rec[:, t])
+
+    print("Plotting sample mean for the protein evolution...")
+    plt.plot(ts, pem, color="orange")
+    plt.plot(ts, pem + std, color="magenta")
+    plt.plot(ts, pem - std, color="green")
+    plt.ylim(0, x0[0])
+    plt.legend(
+        ["Mean of P", f"Mean of P + {std_run} samples std. deviation",
+         f"Mean of P - {std_run} samples std. deviation"])
+    plt.show()
+
 
 def michmen():
-    y0 = [5e-7, 2e-7, 0, 0] # Initial concentrations
+    ls = ["Substrate", "Enzyme", "Complex", "Product"]
     x0 = [301, 120, 0, 0] # Initial number of molecules
     r = [
         [-1, -1, 1, 0], # Binding
@@ -26,22 +107,28 @@ def michmen():
         [0, 1, -1, 1] # Conversion
     ]
     vol = 1e-15 # Volume
-    r1, r2, r3 = 1.66e-3, 1e-4, 0.1 # Reaction constants
+    c1, c2, c3 = 1.66e-3, 1e-4, 0.1 # Reaction constants
 
     def b(x):
-        return r1 * x[0] * x[1]
+        """Binding propensity"""
+        return c1 * x[0] * x[1]
+
     def d(x):
-        return r2 * x[2]
+        """Dissociation propensity"""
+        return c2 * x[2]
+
     def c(x):
-        return r3 * x[2]
+        """Conversion propensity"""
+        return c3 * x[2]
 
-    ssa(x0, r, [b, d, c], "Michael-Menten: SSA")
+    ssa(x0, r, [b, d, c], vol, title="Michael-Menten: SSA", labels=ls)
 
-    elmaru(x0, r, [b, d, c], vol, "Michael-Menten: Euler-Maruyama")
+    elmaru(x0, r, [b, d, c], vol, title="Michael-Menten: Euler-Maruyama", labels=ls)
 
 
 def main():
-    michmen()
+    dimkin()
+    # michmen()
 
 if __name__ == "__main__":
     main()
